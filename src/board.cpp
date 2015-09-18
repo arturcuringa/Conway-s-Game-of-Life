473881ec975d5c14ba9bd2c08b5e784b3dc6022a
Lucasper <lucasmf2005@yahoo.com.br>
2015-09-18 17:38:51 -0300

    modificações

--- src/board.cpp
+++ src/board.cpp
@@ -9,7 +9,7 @@ Board::Board(int n, int m): nLin(n), nCol(m){
  		table[nLin] = new int [nCol];
  	}
 
- 	tableb = new bool* [nLin];
+ 	tableb.state = new bool* [nLin];
  	for (bool i = 0; i < nLin; ++i)
  	{
  		tableb[nLin] = new bool [nCol];
